99c46ef530f01ad70bd611f7eac41a8d699033e0
alexandervidal <57359732+alexandervidal@users.noreply.github.com>
2019-11-04 10:10:02 -0500

    Update SCRIPT_STATUS_CHANGE_ASTERISK_NOTIFICATION.sh

--- SCRIPT_STATUS_CHANGE_ASTERISK_NOTIFICATION.sh
+++ SCRIPT_STATUS_CHANGE_ASTERISK_NOTIFICATION.sh
@@ -3,8 +3,8 @@
 #create: https://alexandervidalolortegui.com
 #Hello friends, This script will help you control the status of the attachments in your aterisk.
 ###########################################################
-#{{status_folder} - Example: /root/house/asterisk_status
-#{sendmail_parameters} - Example sendmail, postfix , phpmailer
+##{{status_folder} - Example: /root/house/asterisk_status
+##{sendmail_parameters} - Example sendmail, postfix , phpmailer
 ############################################################
 #Then tie a contrap
 ###################
